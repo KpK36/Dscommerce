1f86704e92f39d6b91e24ea07a3770ed8a48da17
KpK36 <kayopatryck1@gmail.com>
2023-07-25 23:17:06 -0300

    Mensagem customizada na ArrayList de erros

---
+++ src/main/java/com/devsuperior/dscommerce/dto/FieldMessage.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dscommerce.dto;
+
+public class FieldMessage {
+    private String fieldName;
+    private String message;
+
+    public FieldMessage(String fieldName, String message) {
+        this.fieldName = fieldName;
+        this.message = message;
+    }
+
+}
